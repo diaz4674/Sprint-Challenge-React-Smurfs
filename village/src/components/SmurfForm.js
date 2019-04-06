48047b93d92bb440754d83834195a8740db26b73
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-04-06 11:20:18 -0700

    completed GET, working on POST

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      smurf: {
+        name: '',
+        age: '',
+        height: ''}
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    this.props.updateSmurfs(event, this.state.smurf)
+    this.setState({ name: "", age: "", height: "" })
   }
 
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  handleInputChange = event => {
+    event.persist();
+      this.setState(prevState => {
+        return {
+          smurf: {
+            ...prevState.smurf,
+            [event.target.name]: event.target.value 
+          }
+        }
+      })
   };
 
   render() {
@@ -32,20 +38,23 @@ class SmurfForm extends Component {
           <input
             onChange={this.handleInputChange}
             placeholder="name"
-            value={this.state.name}
+            value={this.state.smurf.name}
             name="name"
+            type = 'text'
           />
           <input
             onChange={this.handleInputChange}
             placeholder="age"
-            value={this.state.age}
+            value={this.state.smurf.age}
             name="age"
+            type = 'number'
           />
           <input
             onChange={this.handleInputChange}
             placeholder="height"
-            value={this.state.height}
+            value={this.state.smurf.height}
             name="height"
+            type = 'text'
           />
           <button type="submit">Add to the village</button>
         </form>
